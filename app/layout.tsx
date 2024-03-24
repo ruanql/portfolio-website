fc8aae9d8b9f3ee9dd64ba91be2b51a2a94fb589
Ruan <ruanqzlt@gmail.com>
2024-03-24 19:06:08 -0300

    Add intro section

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-gray-50  text-gray-950
-      h-[5000px]`}>
+      h-[5000px] pt-28 sm:pt-36`}>
         <div className="bg-[#fbe2e3] absolute top-[-6rem] right-[11rem] 
         -z-10 h-[31.25rem] w-[31.25] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] left-[35rem] 
--- components/intro.tsx
+++ components/intro.tsx
@@ -1,16 +1,59 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import React from "react";
 
 export default function Intro() {
   return (
-    <section>
-        <div className="flex items-center justify-center">
-            <div>
-                <Image src="https://avatars.githubusercontent.com/u/18177535?v=4" 
-                alt="Ruan portrait" width="192" height="192" quality="95" 
-                priority={true} />
-            </div>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="https://avatars.githubusercontent.com/u/18177535?v=4"
+              alt="Ruan portrait"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem]
+                border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
         </div>
+      </div>
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I&apos;m Ruan.</span> I&apos;m a 
+        <span className="font-bold"> full-stack developer</span> with 
+        <span className="font-bold"> 6 years</span> of experience. I enjoy 
+        building <span className="italic">sites & apps</span>. My focus is {" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.h1>
     </section>
-  )
+  );
 }
