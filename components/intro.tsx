110b9b48a1d72e66a22a1472338480420f01c8e8
Ruan <ruanqzlt@gmail.com>
2024-04-03 14:21:08 -0300

    Add intro style

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,11 +1,18 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
+
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
+    <section 
+    id="home"
+    className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -54,6 +61,55 @@ export default function Intro() {
         building <span className="italic">sites & apps</span>. My focus is {" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 
+        px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center 
+          gap-2 rounded-full outline-none focus:scale-105 hover:scale-105
+          hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 
+          rounded-full outline-none focus:scale-105 hover:scale-105 
+          active:scale-105 transition"
+          href="/CV.pdf"
+          download
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex 
+          items-center gap-2 rounded-full"
+          href="https://linkedin.com/in/ruanqueiroz"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 
+          flex items-center gap-2 text-[1.35rem] rounded-full"
+          href="https://github.com/ruanql"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
