8894c572d86603f91eb8ef1fe347bd99f67eac13
Ruan Queiroz Leite <ruanqzlt@gmail.com>
2026-05-05 18:49:05 -0300

    fix: mouse houver button and tsconfig

--- components/intro.tsx
+++ components/intro.tsx
@@ -83,7 +83,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full 
-          outline-none focus:scale-110 hover:scale-110 active:scale-105 
+          outline-none focus:scale-110 hover:scale-105 active:scale-105 
           transition cursor-pointer borderBlack dark:bg-white/10"
           href="/Software_Engineer_Ruan.pdf"
           download
